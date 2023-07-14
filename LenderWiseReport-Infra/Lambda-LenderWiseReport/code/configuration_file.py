871d939b20958279cdb3f621932f56d848e53d68
magicalgates <yashwanths5599@gmail.com>
2023-07-14 13:02:41 +0530

    Update configuration_file.py

--- LenderWiseReport-Infra/Lambda-LenderWiseReport/code/configuration_file.py
+++ LenderWiseReport-Infra/Lambda-LenderWiseReport/code/configuration_file.py
@@ -1,4 +1,4 @@
-#main.py parameter details.
+#main.py parameter details(Common Generic Report)
 generic_report_name = 'NDS_Systems_Generic_LenderWiseCost_Report.xlsx'
 generic_Service_cost_sheet = 'NDS_Systems' 
 generic_Lender_waf_sheet = 'Lender' 
@@ -7,19 +7,19 @@ generic_s3_sheet = 's3 Usage'
 generic_LenderNames_sheet = 'LenderNames'
 
 #Servicecost.py parameter details
-generic_service_cost_file_name = '436415935402_NDS_Systems.xlsx'
+# generic_service_cost_file_name = '436415935402_NDS_Systems.xlsx'
 generic_servicecostsheet = '436415935402_NDS_Systems'
 
 #S3_size.py parameter details
-generic_s3_report_name = 's3 bucket storage count.xlsx'
+# generic_s3_report_name = 's3 bucket storage count.xlsx'
 generic_s3_report_sheet = 's3 bucket storage count'
 
 #db_size.py parameter details
-generic_db_report_name = 'DB_Report.xlsx'
+# generic_db_report_name = 'DB_Report.xlsx'
 generic_db_report_sheet = 'Production Lender'
 
 #Lender_waf_request.py parameter details
-generic_lender_waf_report = 'waf-report.xlsx'
+# generic_lender_waf_report = 'waf-report.xlsx'
 generic_lender_waf_report_sheet = 'Report'
 
 #SES.py details
@@ -30,9 +30,14 @@ email_body = 'Please find the attached report.'                             # Sp
 email_attachment_path = 'NDS_Systems_Generic_LenderWiseCost_Report.xlsx'    # Specify the path of local report file
 
 #s3_import.py paramater details
+#Input values
+# Year = 2023
+Month = 'June'
+s3_folder_prefix='Bills'
+
 generic_bucket_name = 'yashdemobucket007'
-db_file_path = 'Bills/May/DB_Report.xlsx'
-s3_report_file_path = 'Bills/May/s3 bucket storage count.xlsx'
-service_cost_file_path = 'Bills/May/436415935402_NDS_Systems.xlsx'
-Lender_waf_report_file_path = 'Bills/May/waf-report.xlsx'
-generic_report_file_path = 'Bills/May/NDS_Systems_Generic_LenderWiseCost_Report.xlsx'
+db_file_path = s3_folder_prefix + '/' + Month + '/' + 'DB_Report.xlsx'
+s3_report_file_path = s3_folder_prefix + '/' + Month + '/' + 's3 bucket storage count.xlsx'
+service_cost_file_path = s3_folder_prefix + '/' + Month + '/' + '436415935402_NDS_Systems.xlsx'
+Lender_waf_report_file_path = s3_folder_prefix + '/' + Month + '/' + 'waf-report.xlsx'
+generic_report_file_path = s3_folder_prefix + '/' + Month + '/' + 'NDS_Systems_Generic_LenderWiseCost_Report.xlsx'
